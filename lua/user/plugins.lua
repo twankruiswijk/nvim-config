5fdecbc33287a6fa2352b2c079092218b89aa4b0
Twan Kruiswijk <twankrui@gmail.com>
2022-08-31 15:22:28 +0200

    added null ls

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -68,6 +68,7 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig"
   use "williamboman/nvim-lsp-installer"
+  use "jose-elias-alvarez/null-ls.nvim"
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
